1f74c0156fee1f1b5758ed6d722eb362bec8db45
Bohdan Zhydyk <bzua83@gmail.com>
2021-02-21 04:30:20 +0100

    add Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,8 +4,38 @@ import './Footer.scss'
 
 const Footer = ({footer})=>{
   return(
-    <footer className="flex">
-      <span style={{color:'red'}}>Tutaj bede FOOTER...</span>
+    <footer className="flex between">
+
+      <div className="information">
+      {
+        footer.info.map( (line, index)=>{
+          return(
+            line.href
+            ?
+            <a className="infoLine" href={line.href} target="_blank" rel="noreferrer">
+              {line.name}
+            </a>
+            :
+            <div className="infoLine">{line.name}</div>
+          )
+        })
+      }
+      </div>
+
+      <div className="opened">
+        <h3>{footer.opened.txt}</h3>
+      {
+        footer.opened.lines.map( (line, index)=>{
+          return(
+            <div className="flex between">
+              <span>{line.day}</span>
+              <span>{line.hours}</span>
+            </div>
+          )
+        })
+      }
+      </div>
+
     </footer>
   )
 }
