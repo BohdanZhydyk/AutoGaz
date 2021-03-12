c413ba412b2453b99bf72c765ac832b1831f79b5
Bohdan Zhydyk <bzua83@gmail.com>
2021-03-12 14:29:22 +0100

    adminMode for tagHeading

--- client/src/components/TagsParser/Tags/TagHeading.js
+++ client/src/components/TagsParser/Tags/TagHeading.js
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { EditDeletePannel } from './../EditDeletePannel'
+import { AddTagPannel } from './../AddTagPannel'
 
 
-export const TagHeading = ({ item, theme, index, act })=>{
+export const TagHeading = ({ item, theme, index, act, admin })=>{
+
+  const [target, setTarget] = useState(item)
+
+  let edit = ()=> act({ addr:"admin", type:"EDIT_TAG", payload:{inn:target.inn} })
+
   return(
-    <h3 className={`tagHeading tagHeading-${theme} flex`} key={`Heading${index}`} >
-      <span>{item.txt}</span>
+    <h3 className={`tagHeading tagHeading-${theme} ${admin && 'tagHeading-admin'} flex`} >
+    {
+
+      !target.editing
+
+      ?
+
+      <div className="content flex" onClick={ ()=> admin && edit() } >
+        <span>{target.txt}</span>
+      </div>
+
+      :
+      <div className="content flex column" >
+        <AddTagPannel theme={theme} />
+
+        <div className="flex">
+
+          <input type="text" value={target.txt} placeholder={`text hedera...`}
+              onChange={ (e)=> setTarget({...target, txt:e.target.value}) } />
+
+          <EditDeletePannel item={target} theme={theme} act={act} />
+          
+        </div>
+        
+        <AddTagPannel theme={theme} />
+      </div>
+
+    }
     </h3>
   )
   // {
--- client/src/components/TagsParser/Tags/TagIframe.js
+++ client/src/components/TagsParser/Tags/TagIframe.js
@@ -25,3 +25,14 @@ export const TagIframe = ({ item, theme, index, act })=>{
   //   src:string
   // }
 }
+
+
+// <section>
+//   <meter id="fuel" min="0" max="100" low="33" high="66" optimum="80" value="70"></meter>
+// </section>
+
+// <script>
+//   document.querySelector('input[type="rahge"]').oninput = function(){
+//     document.querySelector('meter').value = this.value
+//   }
+// </script>
